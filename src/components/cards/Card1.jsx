e2d5ad1d0ba526bc5b9025e67a77e2a67910a18d
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-07-04 12:36:38 +0530

    card1 formatting

--- src/components/cards/Card1.jsx
+++ src/components/cards/Card1.jsx
@@ -20,12 +20,12 @@ export default function Card1() {
         </div>
       </div>
 
-      <div className='card-content grid h-40 grid-cols-[2.8fr,1.3fr] items-center gap-4 overflow-hidden md:h-48 xl:h-60 xl:grid-cols-[3fr,1fr]'>
-        <div className='h-40 md:h-48 xl:h-60'>
+      <div className='card-content grid h-40 grid-cols-[2.8fr,1.3fr] items-center gap-4 overflow-hidden md:h-48'>
+        <div className='h-40 md:h-48'>
           <h2 className='pb-2 text-lg font-bold !leading-6 md:text-2xl'>
             Long term effects of our increasing disconnect from nature
           </h2>
-          <p className='text-sm text-[var(--text-gray)] xl:pt-1'>
+          <p className='text-sm text-[var(--text-gray)]'>
             Lorem, ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Beatae atque, aperiam quam ipsa vero alias. Fuga,
             corporis optio! Sequi dolores tempora repellendus placeat ipsum
@@ -54,7 +54,7 @@ export default function Card1() {
           </p>
         </div>
         <div
-          className='img-container bg-style h-32 min-w-[130px] overflow-hidden rounded-md  bg-blue-400 md:h-44 xl:h-60'
+          className='img-container bg-style h-32 min-w-[130px] overflow-hidden rounded-md  bg-blue-400 md:h-48'
           style={{
             backgroundImage:
               'url("/assets/(s)mick-haupt-TEjR4zowKgE-unsplash.jpg")',
