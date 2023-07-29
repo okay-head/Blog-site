7be5438f422f6648fe28dfbe1c67072ceaa0a80e
admin-on-win10 <shashwatjaiswal14@gmail.com>
2023-07-29 09:26:48 +0530

    page renders with mock data

--- data/fakeData.cjs
+++ data/fakeData.cjs
@@ -11,27 +11,27 @@ const formatDate = date => format(date, 'Do MMMM, YYYY')
 
 const id = 1
 
-const card = {
-  avatar: null,
-  author: faker.person.fullName(),
-  date: formatDate(
-    faker.date.between({
-      from: '2020-01-01T00:00:00.000Z',
-      to: '2030-01-01T00:00:00.000Z'
-    })
-  ),
-  title:
-    toTitleCase(faker.word.words(1)) +
-    ' ' +
-    faker.word.words({ count: { min: 5, max: 10 } }),
-  body: faker.lorem.paragraphs(5),
-  tags: [faker.word.noun(), faker.word.noun()],
-  hero: null
-}
-
 const jsonData = {
-  data: Array(10).fill(1).map((_,i)=>({id:id+i, ...card}))
+  data: Array(10).fill(1).map((_,i)=>({
+    id:id+i,
+    avatar: null,
+    author: faker.person.fullName(),
+    date: formatDate(
+      faker.date.between({
+        from: '2020-01-01T00:00:00.000Z',
+        to: '2024-01-01T00:00:00.000Z'
+      })
+    ),
+    title:
+      toTitleCase(faker.word.words(1)) +
+      ' ' +
+      faker.word.words({ count: { min: 5, max: 10 } }),
+    body: faker.lorem.paragraphs(5),
+    tags: [faker.word.noun(), faker.word.noun()],
+    hero: null
+  }))
 }
+console.log(jsonData);
 
 fs.writeFile('./db.json', JSON.stringify(jsonData), err=>{
   if (err) 
