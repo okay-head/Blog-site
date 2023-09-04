fd67c4115578684e402228fcc471f5fb1b2e684e
okay-head <68184623+okay-head@users.noreply.github.com>
2023-09-04 21:30:25 +0530

    removed swc | working with vite core

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react' //removed swc
 
 // https://vitejs.dev/config/
 export default defineConfig({
