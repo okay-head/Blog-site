bb498e9b969c4a11f51a3900df4763a5d98c4a5e
okay-head <68184623+okay-head@users.noreply.github.com>
2023-09-06 18:41:55 +0530

    removed unused component

--- src/components/user/ReadingList.jsx
+++
@@ -1,9 +0,0 @@
-import useContextHook from '../../state/useContextHook'
-
-export default function ReadingList() {
-  const {
-    user: { user_bookmarks },
-  } = useContextHook()
-  console.log(`Reading list: User's reading list- `, user_bookmarks)
-  return <div className='pt-24'>ReadingList</div>
-}
