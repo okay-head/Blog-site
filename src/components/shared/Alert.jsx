ddaa372aa094bbbb2ed002c5b5cf50934900b436
okay-head <68184623+okay-head@users.noreply.github.com>
2023-10-01 19:24:42 +0530

    refactoring

--- src/components/shared/Alert.jsx
+++ src/components/shared/Alert.jsx
@@ -1,6 +1,5 @@
 // custom alert box
 // put this here so that it can be shared b/w all the pages
-// just need to pass a trigger function
 
 import { Outlet } from 'react-router-dom'
 import AlertBox from './AlertBox'
