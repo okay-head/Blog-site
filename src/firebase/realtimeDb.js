7e1fb550aba1c435e4d85f4e731249303b504528
okay-head <68184623+okay-head@users.noreply.github.com>
2023-10-19 20:20:24 +0530

    tested with local emulator suite

--- src/firebase/realtimeDb.js
+++ src/firebase/realtimeDb.js
@@ -7,79 +7,100 @@ import {
   update,
   remove,
 } from 'firebase/database'
+import { app } from './firebaseApp'
 
-export default function realtimeDb(app) {
-  const db = getDatabase(app)
+const db = getDatabase(app)
 
-  connectDatabaseEmulator(db, 'localhost', 9000)
+connectDatabaseEmulator(db, 'localhost', 9000)
 
-  // ----| GET |----
-  const getFn = async (url, check) => {
-    try {
-      const res = await get(ref(db, url))
-      if (check) return res.exists()
+// ----| GET |----
+const getFn = async (url, check) => {
+  try {
+    const res = await get(ref(db, url))
+    if (check) return res.exists()
 
-      console.log(res.exists() && res.val())
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-
-  // ----| SET |----
-  const setUserFn = async (data) => {
-    try {
-      // check for the resource just in case
-      const exists = await getFn(ref(db, url), true)
-      if (exists)
-        console.warn('Already exists. Create unique id and try again.')
-
-      await set(ref(db, 'user/' + data.id), data)
-      console.log('Resource set successfully!')
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-  const setDataFn = async (data) => {
-    try {
-      // find the count value for id
-      const currentCounter = (await get(ref(db, '/count'))).val()
-      data.id = ++currentCounter
-      await set(ref(db, 'data/' + data.id), data)
-
-      //update counter
-      await update(ref(db, '/count'), data.id)
-      console.log('Resource set successfully!')
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-
-  const patchFn = async (url, data) => {
-    try {
-      // find if resource exists
-      const exists = await getFn(url, true)
-      if (!exists) {
-        console.warn('Resource doesnt exist!')
-        return
-      }
-      await update(ref(db, url), data)
-      console.log('Updated successfully')
-    } catch (error) {
-      throw new Error(error)
-    }
-  }
-  const deleteFn = async (url) => {
-    try {
-      // find if resource exists
-      const exists = await getFn(url, true)
-      if (!exists) {
-        console.warn('Resource doesnt exist!')
-        return
-      }
-      await remove(ref(db, url))
-      console.log('Resource deleted!')
-    } catch (error) {
-      throw new Error(error)
-    }
+    console.log(res.exists() && res.val())
+  } catch (error) {
+    throw new Error(error)
   }
 }
+// only for getting all data
+// no fn for user since no use case for the same
+const getAllDataFn = async (url) => {
+  try {
+    const res = await get(ref(db, url))
+
+    if (!res.exists()) {
+      console.warn('No data present!')
+      return
+    }
+    res.val().forEach((x, i) => {
+      if (i !== 0) console.log(x)
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+// ----| SET |----
+const setUserFn = async (data) => {
+  try {
+    // check for the resource just in case
+    const url = 'users/' + data.id
+    const exists = await getFn(url, true)
+    if (exists) {
+      console.warn('Already exists. Create unique id and try again.')
+      return
+    }
+
+    await set(ref(db, 'users/' + data.id), data)
+    console.log('Resource set successfully!')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+const setDataFn = async (data) => {
+  try {
+    // find the count value for id
+    let currentCounter = (await get(ref(db, '/count'))).val()
+    data.id = ++currentCounter
+    await set(ref(db, 'data/' + data.id), data)
+
+    //update counter
+    await update(ref(db, '/count'), data.id)
+    console.log('Resource set successfully!')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const patchFn = async (url, data) => {
+  try {
+    // find if resource exists
+    const exists = await getFn(url, true)
+    if (!exists) {
+      console.warn('Resource doesnt exist!')
+      return
+    }
+    await update(ref(db, url), data)
+    console.log('Updated successfully')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+const deleteFn = async (url) => {
+  try {
+    // find if resource exists
+    const exists = await getFn(url, true)
+    if (!exists) {
+      console.warn('Resource doesnt exist!')
+      return
+    }
+    await remove(ref(db, url))
+    console.log('Resource deleted!')
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export { getFn, getAllDataFn, setUserFn, setDataFn, patchFn, deleteFn }
